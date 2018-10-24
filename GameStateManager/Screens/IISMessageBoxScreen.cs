443ade3a929ad596a03ca418525d37f327d42ded
Rafael Zacharias <rafaelrzacharias@gmail.com>
2018-10-24 22:21:54 +0100

    Implemented the IISMessageBoxScreen to be used as a popup message anytime a controller scheme has changed, is disconnected, or during the initial screen interaction.

---
+++ GameStateManager/Screens/IISMessageBoxScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace GameStateManager
+{
+    public class IISMessageBoxScreen : MessageBoxScreen
+    {
+        // Constructs an initialInteraction screen.
+        public IISMessageBoxScreen() :
+            base("Press any key to start", false)
+        {
+            ShouldDarkenBackground = false;
+            OnShow();
+        }
+
+
+        public override void HandleInput()
+        {
+            if (Input.WasAnyButtonPressed())
+            {
+                List<string> screens = new List<string> { "MainMenuScreen" };
+                LoadingScreen.Load(false, screens);
+            }
+        }
+    }
+}
