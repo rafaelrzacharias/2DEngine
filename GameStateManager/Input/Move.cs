a4b317baeeaa2487409a11d63bda7077263c8bd0
Rafael Zacharias <rafaelrzacharias@gmail.com>
2018-11-21 21:53:06 +0000

    Integration of the support classes used in the buffered input system.

---
+++ GameStateManager/Input/Move.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace GameStateManager
+{
+    // Describes a sequences of buttons which must be pressed to activate the move.
+    public class Move : IComparable
+    {
+        public string Name;
+
+        // The sequence of button presses required to activate this move.
+        public Buttons[] Sequence;
+
+        // Set this to true if the input used to activate this move
+        // may be reused as a component of longer moves.
+        public bool IsSubMove;
+
+
+        public Move(string name, params Buttons[] sequence)
+        {
+            Name = name;
+            Sequence = sequence;
+        }
+
+
+        // Default implementation of IComparable.
+        public int CompareTo(object obj)
+        {
+            return Sequence.Length;
+        }
+
+
+        // A real game would implement this method in a derived class.
+        public void PerformMove(string name) { }
+    }
+}
---
+++ GameStateManager/Input/MoveList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GameStateManager
+{
+    // Represents a set of available moves for matching. This internal storage
+    // of this class is optimized for efficient match searches.
+    public class MoveList
+    {
+        Move[] moves;
+
+        public MoveList(Move[] moves)
+        {
+            // Store the list of moves in order of decreasing sequence length.
+            // This greatly simplifies the logic of the DetectMove method.
+            Array.Sort(moves);
+            Array.Reverse(moves);
+            this.moves = moves;
+        }
+
+
+        // Finds the longest Move which matches the given input, if any.
+        public Move DetectMoves(int controllerIndex)
+        {
+            // Perform a linear search for a move which matches the input. This relies
+            // on the moves array being in order of decreasing sequence length.
+            for (int i = 0; i < moves.Length; i++)
+            {
+                if (Input.Matches(moves[i], controllerIndex))
+                    return moves[i];
+            }
+
+            return null;
+        }
+
+
+        public int LongestMoveLength
+        {
+            // Since they are in decreasing order, the first move is the longest.
+            get { return moves[0].Sequence.Length; }
+        }
+    }
+}
