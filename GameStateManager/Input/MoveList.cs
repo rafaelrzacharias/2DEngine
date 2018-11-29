7f875bd457f6d07f83f5cce5c6b73aac61ebb671
CyberMonkey <rafaelrzacharias@gmail.com>
2018-11-29 18:21:47 +0000

    Added a list of moves to the clas constructor. Added a Name for the move list, a length and an overloaded index operator to retrieve elements from the list.

--- GameStateManager/Input/MoveList.cs
+++ GameStateManager/Input/MoveList.cs
@@ -6,14 +6,30 @@ namespace GameStateManager
     // of this class is optimized for efficient match searches.
     public class MoveList
     {
+        public string Name { get; private set; }
         private Move[] Moves;
 
-        public MoveList(Move[] moves)
+        public MoveList(string name = "")
         {
+            if (string.IsNullOrEmpty(name) == false)
+                Name = name;
+
+            Moves = new Move[]
+                {
+                new Move("Jump", Action.LK) { IsSubMove = true },
+                new Move("Punch", Action.LP) { IsSubMove = true },
+                new Move("Double Jump", Action.LK, Action.LK),
+                new Move("Jump Kick", Action.LK | Action.LK),
+                new Move("Quad Punch", Action.LP, Action.HP, Action.LP, Action.HP),
+                new Move("Fireball", Action.DOWN, Action.DOWN_RIGHT, Action.RIGHT | Action.LP),
+                new Move("Long Jump", Action.UP, Action.UP, Action.LK),
+                new Move("Back Flip", Action.DOWN, Action.DOWN | Action.LK),
+                new Move("30 Lives", Action.UP, Action.UP, Action.DOWN, Action.DOWN, Action.LEFT, Action.RIGHT, Action.LEFT, Action.RIGHT, Action.HK, Action.LK),
+                };
+
             // Store the list of moves in order of decreasing sequence length.
             // This greatly simplifies the logic of the DetectMove method.
-            Array.Sort(moves);
-            Moves = moves;
+            Array.Sort(Moves);
         }
 
 
@@ -37,5 +53,10 @@ namespace GameStateManager
             // Since they are in decreasing order, the first move is the longest.
             get { return Moves[0].Sequence.Length; }
         }
+
+
+        public int Length { get { return Moves.Length; } }
+
+        public Move this[int i] { get { return Moves[i]; } }
     }
 }
