e6c60609704602835458c5c9e708f97e376bd998
Rafael Zacharias <rafaelrzacharias@gmail.com>
2018-12-11 22:56:28 +0000

    Moved the ActionMap out of the Input system and into its own class.

---
+++ GameStateManager/Input/ActionMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+
+namespace GameStateManager
+{
+    // A combination of gamepad and keyboard keys mapped to a particular action.
+    public class ActionMap
+    {
+#if DESKTOP
+        public List<Keys> Keys = new List<Keys>();
+        public List<MouseButtons> MouseButtons = new List<MouseButtons>();
+#endif
+#if DESKTOP || CONSOLE
+        public List<Buttons> Buttons = new List<Buttons>();
+#endif
+    }
+}
