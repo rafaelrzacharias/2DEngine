d6e5825fbb4fce9a296f919c6c6cee12ad9682f4
Rafael Zacharias <rafaelrzacharias@gmail.com>
2018-12-11 22:55:04 +0000

    Implementation of a new save input message box for when players need to save a different input scheme.

---
+++ GameStateManager/Screens/SaveInputMapMessageBoxScreen.cs
@@ -0,0 +1,12 @@
+﻿
+namespace GameStateManager
+{
+    public class SaveInputMapMessageBoxScreen : MessageBoxScreen
+    {
+        public SaveInputMapMessageBoxScreen(string screenName, string message, string menuTitle) :
+            base(screenName, message, menuTitle, MessageBoxType.YESNO)
+        {
+            
+        }
+    }
+}
