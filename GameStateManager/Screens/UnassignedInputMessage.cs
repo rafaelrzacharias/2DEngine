1979c8489252e4552442b37e6f12cdfec644a3c5
Rafael Zacharias <rafaelrzacharias@gmail.com>
2018-12-11 22:58:34 +0000

    Implemented a small message box that warns players of unassigned input.

---
+++ GameStateManager/Screens/UnassignedInputMessage.cs
@@ -0,0 +1,12 @@
+﻿
+namespace GameStateManager
+{
+    public class UnassignedInputMessage : MessageBoxScreen
+    {
+        public UnassignedInputMessage(string screenName, string message, string menuTitle) :
+           base(screenName, message, menuTitle, MessageBoxType.OK)
+        {
+            
+        }
+    }
+}
