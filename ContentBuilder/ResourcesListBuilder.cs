f0fe5b6501d0141d4e73536aa1491bb33e1776a3
CyberMonkey <rafaelrzacharias@gmail.com>
2018-12-13 12:15:24 +0000

    ContentBuilder project library has been rebuilt and now fully works! This is the initial step to implement all content importers and processors for data classes in the game. The idea is: Importers and Processors will be implemented in the ContentBuilder DLL library (to be used by the ContentPipeline) and the class that specifies the object data itself will contain the Reader and Writer as additional classes (in the GameProject).

--- ContentBuilder/ResourcesListBuilder.cs
+++ ContentBuilder/ResourcesListBuilder.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Xml;
 using Microsoft.Xna.Framework.Content.Pipeline;
-using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
-using Microsoft.Xna.Framework.Content;
 using GameStateManager;
+using System.Collections.Generic;
 
 namespace ContentBuilder
 {
     // The Content Importer’s main task is to read the contents
     // of the file and pass it to the content processor.
-    [ContentImporter(".xml", DefaultProcessor = "ResourcesListProcessor", DisplayName = "ResourcesList Importer")]
+    [ContentImporter(".res", DefaultProcessor = "ResourcesListProcessor", DisplayName = "ResourcesList Importer")]
     public class ResourcesListImporter : ContentImporter<XmlDocument>
     {
         public override XmlDocument Import(string filename, ContentImporterContext context)
@@ -35,40 +34,49 @@ namespace ContentBuilder
         {
             try
             {
-                context.Logger.LogMessage("Started processing RES file: " + context.OutputFilename);
-                context.Logger.LogMessage("Processing songs...");
+                List<string> songs = new List<string>();
+                List<string> soundEffects = new List<string>();
+                List<string> fonts = new List<string>();
+                List<string> textures = new List<string>();
 
-                XmlNodeList songsList = input.GetElementsByTagName("Songs");
-                string[] songs = new string[songsList.Count];
+                XmlNode root = input.DocumentElement.FirstChild;
+                XmlNodeList nodes = root.ChildNodes;
 
-                for (int i = 0; i < songs.Length; i++)
-                    songs[i] = songsList[i].Name;
+                foreach (XmlNode node in nodes)
+                {
+                    if (node.Name == "Songs")
+                    {
+                        XmlNodeList songsNode = node.ChildNodes;
 
-                context.Logger.LogMessage("Processing soundEffects...");
+                        for (int i = 0; i < songsNode.Count; i++)
+                            songs.Add(songsNode[i].InnerText);
+                    }
 
-                XmlNodeList soundEffectsList = input.GetElementsByTagName("SoundEffects");
-                string[] soundEffects = new string[soundEffectsList.Count];
+                    if (node.Name == "SoundEffects")
+                    {
+                        XmlNodeList soundEffectsNode = node.ChildNodes;
 
-                for (int i = 0; i < soundEffects.Length; i++)
-                    soundEffects[i] = soundEffectsList[i].Name;
+                        for (int i = 0; i < soundEffectsNode.Count; i++)
+                            soundEffects.Add(soundEffectsNode[i].InnerText);
+                    }
 
-                context.Logger.LogMessage("Processing fonts...");
+                    if (node.Name == "Fonts")
+                    {
+                        XmlNodeList fontsNode = node.ChildNodes;
 
-                XmlNodeList fontsList = input.GetElementsByTagName("Fonts");
-                string[] fonts = new string[fontsList.Count];
+                        for (int i = 0; i < fontsNode.Count; i++)
+                            fonts.Add(fontsNode[i].InnerText);
+                    }
 
-                for (int i = 0; i < fonts.Length; i++)
-                    fonts[i] = fontsList[i].Name;
+                    if (node.Name == "Textures")
+                    {
+                        XmlNodeList texturesNode = node.ChildNodes;
 
-                context.Logger.LogMessage("Processing textures...");
+                        for (int i = 0; i < texturesNode.Count; i++)
+                            textures.Add(texturesNode[i].InnerText);
+                    }
+                }           
 
-                XmlNodeList texturesList = input.GetElementsByTagName("Textures");
-                string[] textures = new string[texturesList.Count];
-
-                for (int i = 0; i < textures.Length; i++)
-                    textures[i] = texturesList[i].Name;
-
-                context.Logger.LogMessage("ResourcesList processed successfully!");
 
                 ResourcesList resourcesList = new ResourcesList();
                 resourcesList.Songs = songs;
@@ -83,92 +91,6 @@ namespace ContentBuilder
                 context.Logger.LogMessage("Error: " + ex);
                 throw;
             }
-        }
-    }
-
-
-    // The ContentWriter writes the values into the content file.
-    [ContentTypeWriter]
-    public class ResourcesListWriter : ContentTypeWriter<ResourcesList>
-    {
-        protected override void Write(ContentWriter output, ResourcesList value)
-        {
-            int length = value.Songs.Length;
-            output.Write(length);
-
-            for (int i = 0; i < length; i++)
-                output.Write(value.Songs[i].ToString());
-
-            length = value.SoundEffects.Length;
-            output.Write(length);
-
-            for (int i = 0; i < length; i++)
-                output.Write(value.SoundEffects[i].ToString());
-
-            length = value.Fonts.Length;
-            output.Write(length);
-
-            for (int i = 0; i < length; i++)
-                output.Write(value.Fonts[i].ToString());
-
-            length = value.Textures.Length;
-            output.Write(length);
-
-            for (int i = 0; i < length; i++)
-                output.Write(value.Textures[i].ToString());
-        }
-
-
-        public override string GetRuntimeType(TargetPlatform targetPlatform)
-        {
-            return typeof(ResourcesList).AssemblyQualifiedName;
-        }
-
-
-        public override string GetRuntimeReader(TargetPlatform targetPlatform)
-        {
-            return typeof(ResourcesListReader).AssemblyQualifiedName;
-        }
-    }
-
-
-    // The ContentReader is able to construct the object
-    // from the datastream the ContentWriter had written.
-    public class ResourcesListReader : ContentTypeReader<ResourcesList>
-    {
-        protected override ResourcesList Read(ContentReader input, ResourcesList existingInstance)
-        {
-            int length = input.ReadInt32();
-            string[] songs = new string[length];
-
-            for (int i = 0; i < length; i++)
-                songs[i] = input.ReadString();
-
-            length = input.ReadInt32();
-            string[] soundEffects = new string[length];
-
-            for (int i = 0; i < length; i++)
-                soundEffects[i] = input.ReadString();
-
-            length = input.ReadInt32();
-            string[] fonts = new string[length];
-
-            for (int i = 0; i < length; i++)
-                fonts[i] = input.ReadString();
-
-            length = input.ReadInt32();
-            string[] textures = new string[length];
-
-            for (int i = 0; i < length; i++)
-                textures[i] = input.ReadString();
-
-            ResourcesList resourcesList = new ResourcesList();
-            resourcesList.Songs = songs;
-            resourcesList.SoundEffects = soundEffects;
-            resourcesList.Fonts = fonts;
-            resourcesList.Textures = textures;
-
-            return resourcesList;
         }
     }
 }
