2ebdb5d091022eac827af8065ac0e6f0411e4311
Rafael Zacharias <rafaelrzacharias@gmail.com>
2018-12-24 12:27:39 +0000

    Moved the ActionState out of the Input class and into its own class. Added the ActionState class to the compilation process.

---
+++ GameStateManager/Input/ActionState.cs
@@ -0,0 +1,21 @@
+﻿namespace GameStateManager
+{
+    // A struct that represents an input state from a controller.
+    public struct ActionState
+    {
+        public bool IsPressed; // If the action is being pressed.
+        public bool IsTriggered; // If the action was just pressed this frame.
+        public float Magnitude; // The action's intensity (for analog buttons).
+        public double Duration; // How long the action has being pressed for.
+
+
+        // Reset all fields to its default values.
+        public void Reset()
+        {
+            IsPressed = false;
+            IsTriggered = false;
+            Magnitude = 0.0f;
+            Duration = 0.0;
+        }
+    }
+}
