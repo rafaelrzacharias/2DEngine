23b25a4028f6f9280d0e6afc09d2a82853cf9afd
Rafael Zacharias <rafaelrzacharias@gmail.com>
2018-12-24 12:32:32 +0000

    - Now all controllers can vibrate given a duration, magnitude, motor, vibration type and intermittence type :)
    Added MotorType to specify which motor to vibrate.
    Added VibratorMode to specify the kind of vibration and the intermittency to be applied.
    Added all necessary vibration fields to the Controller class (all of this needs to be made private!!!)
    For efficiency, the vibrations will only update if there is a vibration going on and if we are not paused.
    Added the Vibrate, UpdateVibrations, ApplyIntermittence and StopVibration to the Controller class.

--- GameStateManager/Input/Controller.cs
+++ GameStateManager/Input/Controller.cs
@@ -14,11 +14,46 @@ namespace GameStateManager
     }
 
 
+    public enum MotorType
+    {
+        BOTH,
+        LEFT,
+        RIGHT
+    }
+
+
+    public enum VibratorMode
+    {
+        NONE,
+        CONSTANT,
+        INCREASING,
+        DECREASING
+    }
+
+
     // Represent a controller. It can be aassigned to a player or an AI agent.
     public class Controller
     {
         public int Slot;
         public bool IsPrimaryUser;
+        public double LeftMotorTotalDuration;
+        public double RightMotorTotalDuration;
+        public VibratorMode LeftMotorMode;
+        public VibratorMode RightMotorMode;
+        public float LeftMotorInitialMagnitude;
+        public float RightMotorInitialMagnitude;
+        public float leftMotorCurrentMagnitude;
+        public float rightMotorCurrentMagnitude;
+        public double leftMotorRemainingDuration;
+        public double rightMotorRemainingDuration;
+        public VibratorMode LeftIntermittenceMode;
+        public VibratorMode RightIntermittenceMode;
+        public double leftIntermittenceTimer;
+        public double rightIntermittenceTimer;
+        public double leftResetTimer;
+        public double rightResetTimer;
+        public static readonly double MIN_RESET_TIME = 0.1;
+        public bool PauseVibration;
         public ControllerType Type;
         public ActionMap[] ActionMaps;
         public ActionState[] ActionStates;
@@ -30,8 +65,7 @@ namespace GameStateManager
         public Controller()
         {
             Slot = -1;
-            IsPrimaryUser = false;
-            Type = ControllerType.NONE;
+
             ActionMaps = new ActionMap[Input.Actions.Length];
             Input.ResetActionMaps(ActionMaps);
 
@@ -80,6 +114,9 @@ namespace GameStateManager
 
                             ActionStates[i].Reset();
                         }
+
+                        if (PauseVibration == false && (RightMotorMode != VibratorMode.NONE || LeftMotorMode != VibratorMode.NONE))
+                            UpdateVibrations(elapsedTime);
                     }
                     break;
                 case ControllerType.TOUCH:
@@ -207,6 +244,208 @@ namespace GameStateManager
                     return Input.CurrentGamePadState[Slot].ThumbSticks.Right.X;
                 default:
                     return 1.0f;
+            }
+        }
+
+
+        // Sets the controller vibration, given a dration, magnitude, motor type and vibration mode.
+        public void Vibrate(double duration, float magnitude, MotorType motorType, 
+            VibratorMode vibrationMode, VibratorMode intermittenceMode)
+        {
+            switch (motorType)
+            {
+                case MotorType.LEFT:
+                    {
+                        LeftMotorTotalDuration = duration;
+                        leftMotorRemainingDuration = LeftMotorTotalDuration;
+                        LeftMotorInitialMagnitude = magnitude;
+                        LeftMotorMode = vibrationMode;
+                        LeftIntermittenceMode = intermittenceMode;
+                    }
+                    break;
+                case MotorType.RIGHT:
+                    {
+                        RightMotorTotalDuration = duration;
+                        rightMotorRemainingDuration = RightMotorTotalDuration;
+                        RightMotorInitialMagnitude = magnitude;
+                        RightMotorMode = vibrationMode;
+                        RightIntermittenceMode = intermittenceMode;
+                    }
+                    break;
+                case MotorType.BOTH:
+                    {
+                        LeftMotorTotalDuration = duration;
+                        leftMotorRemainingDuration = LeftMotorTotalDuration;
+                        LeftMotorInitialMagnitude = magnitude;
+                        LeftMotorMode = vibrationMode;
+                        LeftIntermittenceMode = intermittenceMode;
+
+                        RightMotorTotalDuration = duration;
+                        rightMotorRemainingDuration = RightMotorTotalDuration;
+                        RightMotorInitialMagnitude = magnitude;
+                        RightMotorMode = vibrationMode;
+                        RightIntermittenceMode = intermittenceMode;
+                    }
+                    break;
+            }
+        }
+
+
+        // Updates the vibration of the left motor at each frame.
+        private void UpdateVibrations(double elapsedTime)
+        {
+            int a = 0;
+            float b = 0;
+
+            int li = 1;
+            leftMotorRemainingDuration -= elapsedTime;
+            leftMotorCurrentMagnitude = 0f;
+
+            switch (LeftMotorMode)
+            {
+                case VibratorMode.INCREASING:
+                    a = 1;
+                    b = 1 - LeftMotorInitialMagnitude;
+                    break;
+                case VibratorMode.DECREASING:
+                    a = -1;
+                    b = LeftMotorInitialMagnitude;
+                    break;
+            }
+
+            leftMotorCurrentMagnitude = LeftMotorInitialMagnitude + a * b * (float)System.Math.Sqrt((LeftMotorTotalDuration - leftMotorRemainingDuration) / LeftMotorTotalDuration);
+
+            if (LeftIntermittenceMode != VibratorMode.NONE)
+                li = ApplyIntermittence(elapsedTime, LeftIntermittenceMode, LeftMotorTotalDuration, leftMotorRemainingDuration, ref leftIntermittenceTimer, ref leftResetTimer);
+
+            if (leftMotorRemainingDuration <= 0.0)
+                StopVibration(MotorType.LEFT);
+
+            a = 0;
+            b = 0;
+
+            int ri = 1;
+            rightMotorRemainingDuration -= elapsedTime;
+            rightMotorCurrentMagnitude = 0f;
+
+            switch (RightMotorMode)
+            {
+                case VibratorMode.INCREASING:
+                    a = 1;
+                    b = 1 - RightMotorInitialMagnitude;
+                    break;
+                case VibratorMode.DECREASING:
+                    a = -1;
+                    b = RightMotorInitialMagnitude;
+                    break;
+            }
+
+            rightMotorCurrentMagnitude = RightMotorInitialMagnitude + a * b * (float)System.Math.Sqrt((RightMotorTotalDuration - rightMotorRemainingDuration) / RightMotorTotalDuration);
+
+            if (RightIntermittenceMode != VibratorMode.NONE)
+                ri = ApplyIntermittence(elapsedTime, RightIntermittenceMode, RightMotorTotalDuration, rightMotorRemainingDuration, ref rightIntermittenceTimer, ref rightResetTimer);
+
+            if (rightMotorRemainingDuration <= 0.0)
+                StopVibration(MotorType.RIGHT);
+
+            System.Console.WriteLine("Vibrating slot: " + Slot.ToString());
+            GamePad.SetVibration(Slot, leftMotorCurrentMagnitude * li, rightMotorCurrentMagnitude * ri);
+        }
+
+
+        // Applies quick pauses to the motor during the duration of the vibration.
+        private int ApplyIntermittence(double elapsedTime, VibratorMode intermittenceMode, 
+            double motorTotalDuration, double motorRemainingDuration, ref double timer, ref double resetTimer)
+        {
+            timer += elapsedTime;
+
+            int a = 0;
+            float b = 0;
+
+            float initialThreshold = MathHelper.Lerp((float)MIN_RESET_TIME, (float)motorTotalDuration, 0.1f);
+
+            switch (intermittenceMode)
+            {
+                case VibratorMode.INCREASING:
+                    a = 1;
+                    b = 1 - initialThreshold;
+                    break;
+                case VibratorMode.DECREASING:
+                    a = -1;
+                    b = initialThreshold;
+                    break;
+            }
+
+            double threshold = initialThreshold + a * b * System.Math.Sqrt((motorTotalDuration - motorRemainingDuration) / motorTotalDuration);
+
+            if (threshold < MIN_RESET_TIME)
+                threshold = MIN_RESET_TIME;
+
+            if (timer > threshold)
+            {
+                resetTimer += elapsedTime;
+
+                if (resetTimer > MIN_RESET_TIME)
+                {
+                    resetTimer = 0.0;
+                    timer = 0.0;
+                }
+                else
+                    return 0;
+            }
+
+            return 1;
+        }
+
+        public void StopVibration(MotorType motorType = MotorType.BOTH)
+        {
+            switch (motorType)
+            {
+                case MotorType.LEFT:
+                    {
+                        LeftMotorInitialMagnitude = 0f;
+                        LeftMotorTotalDuration = 0.0;
+                        leftMotorRemainingDuration = 0.0;
+                        leftMotorCurrentMagnitude = 0f;
+                        LeftIntermittenceMode = VibratorMode.NONE;
+                        LeftMotorMode = VibratorMode.NONE;
+                        leftIntermittenceTimer = 0.0;
+                        leftResetTimer = 0.0;
+                    }
+                    break;
+                case MotorType.RIGHT:
+                    {
+                        RightMotorInitialMagnitude = 0f;
+                        RightMotorTotalDuration = 0.0;
+                        rightMotorRemainingDuration = 0.0;
+                        rightMotorCurrentMagnitude = 0f;
+                        RightMotorMode = VibratorMode.NONE;
+                        RightIntermittenceMode = VibratorMode.NONE;
+                        rightIntermittenceTimer = 0.0;
+                        rightResetTimer = 0.0;
+                    }
+                    break;
+                case MotorType.BOTH:
+                    {
+                        LeftMotorInitialMagnitude = 0f;
+                        LeftMotorTotalDuration = 0.0;
+                        leftMotorRemainingDuration = 0.0;
+                        leftMotorCurrentMagnitude = 0f;
+                        LeftIntermittenceMode = VibratorMode.NONE;
+                        LeftMotorMode = VibratorMode.NONE;
+                        leftIntermittenceTimer = 0.0;
+                        leftResetTimer = 0.0;
+
+                        RightMotorInitialMagnitude = 0f;
+                        RightMotorTotalDuration = 0.0;
+                        rightMotorRemainingDuration = 0.0;
+                        rightMotorCurrentMagnitude = 0f;
+                        RightMotorMode = VibratorMode.NONE;
+                        RightIntermittenceMode = VibratorMode.NONE;
+                        rightIntermittenceTimer = 0.0;
+                        rightResetTimer = 0.0;
+                    }
+                    break;
             }
         }
     }
